ecd24a96c59e6180a7e9652ce59af4dc8a702f79
mshchts <markoshchutskyy@gmail.com>
2025-07-22 21:15:22 +0200

    Update link of rebuilt front end part, dist/index.html, dist/assets/index.js. Remove cors node module, change baseUrl = '/api/notes', add strict mode to main.jsx

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 let notes = [
   {
@@ -21,7 +21,7 @@ let notes = [
 ];
 
 app.use(express.static('dist'));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get('/', (request, response) => {
