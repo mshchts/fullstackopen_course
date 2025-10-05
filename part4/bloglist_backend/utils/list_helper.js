c456f24330bf54eebaeae9fdfbb81570c74653b5
marchell <markoshchutskyy@gmail.com>
2025-10-05 23:21:38 +0200

    Exercises 4.3 and 4.4

---
+++ part4/bloglist_backend/utils/list_helper.js
@@ -0,0 +1,20 @@
+const dummy = (blogs) => {
+  return 1;
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    return blogs.reduce(function(result, blog){
+      return result + blog.likes
+    }, 0)
+  }
+} 
+
+module.exports = {
+  dummy,
+  totalLikes 
+}
